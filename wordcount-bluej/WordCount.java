da35ce5ca32c1920ad5b9aef565b7351cfb41128
B Kleinen <kleinen@infrastructure.de>
2020-01-21 13:23:01 +0100

    imports

--- wordcount-bluej/WordCount.java
+++ wordcount-bluej/WordCount.java
@@ -1,3 +1,9 @@
+
+
+import java.io.*;
+import java.nio.*;
+import java.nio.charset.*;
+import java.nio.file.*;
 public class WordCount{
 
     public static void main(String[] args){
@@ -8,11 +14,12 @@ public class WordCount{
     }
 
     public void readFileStuff(){
-        Charset charset =  Charset.forName("UTF-8");
-        Path path = Paths.get("file");
-        try(BufferedReader reader = 
-            Files.newBufferedReader(path, charset)) {
-            // use reader to process the file
-        }
+        Charset charset = Charset.forName("UTF-8");
+        Path path = Paths.get("afile.txt");
+
+        BufferedReader reader = 
+            Files.newBufferedReader(path, charset);
+                // use reader to process the file
+
     }
 }
