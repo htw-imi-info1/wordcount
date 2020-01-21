4f280ca09dffa03f8a5321b0127a8fee5209ee21
B Kleinen <kleinen@infrastructure.de>
2020-01-21 13:04:27 +0100

    bluej

---
+++ wordcount-bluej/WordCount.java
@@ -0,0 +1,10 @@
+public class WordCount{
+
+    public static void main(String[] args){
+
+        if (args.length < 1){
+            System.out.println("usage: java WordCount <filename>");
+        }
+    }
+
+}
